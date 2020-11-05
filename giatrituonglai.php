76fe14997d73f4e3800563c24b087502629280c5
tongdung13 <tongvanthienvu@gmail.com>
2020-11-05 17:13:47 +0700

    update

---
+++ giatrituonglai.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; chatset=utf-8"/>
+<head>
+<body>
+<form method="post">
+    <input type="text" name="giatrihientai" size="30" placeholder="số tiền đầu tư"/>
+    <input type="text" name="laisuat" size="30" placeholder="lãi suất"/>
+    <input type="text" name="giatrituonglai" size="30" placeholder="số năm đầu tư">
+    <input type="submit" value="check"/>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $giatrihientai = $_POST["giatrihientai"];
+        $laisuat = $_POST["laisuat"];
+        $giatrituonglai = $_POST["giatrituonglai"];
+        $tong = $_POST["tong"];
+
+        for ($i = 0; $i < count($giatrituonglai); $i++) {
+            if ($giatrituonglai == "1") {
+                $tong = $giatrihientai + ($giatrihientai * $laisuat);
+                echo $tong;
+            }
+        }
+    }
+    ?>
+
+</form>
+</body>
+</head>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="Content-Type" content="text/html; utf-8" />
+<head>
+
+</head>
+<body>
+
+</body>
+</html>
