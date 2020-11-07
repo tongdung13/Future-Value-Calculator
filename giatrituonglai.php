9848e0d8f502d20ce3ba0500926c8714f1937ea4
tongdung13 <tongvanthienvu@gmail.com>
2020-11-07 10:51:35 +0700

    ok

--- giatrituonglai.php
+++ giatrituonglai.php
@@ -7,22 +7,19 @@
     <input type="text" name="giatrihientai" size="30" placeholder="số tiền đầu tư"/>
     <input type="text" name="laisuat" size="30" placeholder="lãi suất"/>
     <input type="text" name="giatrituonglai" size="30" placeholder="số năm đầu tư">
-    <input type="submit" value="check"/>
+    <input type="submit" value="check"/><br><br>
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $giatrihientai = $_POST["giatrihientai"];
         $laisuat = $_POST["laisuat"];
         $giatrituonglai = $_POST["giatrituonglai"];
         $tong = $_POST["tong"];
-        $giatrituonglai = 0;
 
-            if ($giatrituonglai += 1) {
-                $tong = $giatrihientai + ($giatrihientai * $laisuat);
-
-            }
-
+        $tong = $giatrihientai + ($giatrihientai * $laisuat);
+        $tong = $tong * $giatrituonglai;
+        echo $tong;
         }
-    echo $tong;
+
     ?>
 
 </form>
